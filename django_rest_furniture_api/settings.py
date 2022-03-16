a9897997c3f0fd82204aeb2c503a5f9c4be87223
Victoria Le <victoriale@Victorias-MacBook-Pro.local>
2022-03-16 13:54:46 -0400

    allowed_hosts from all sources

--- django_rest_furniture_api/settings.py
+++ django_rest_furniture_api/settings.py
@@ -36,10 +36,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [
-    'locahost',
-    'furnituredjango.herokuapp.com',
-]
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
