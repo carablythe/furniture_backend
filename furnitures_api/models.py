a2b72caffc7a642d9adb7d0a33fdf0f011ada674
Victoria Le <victoriale@Victorias-MacBook-Pro.local>
2022-03-17 14:10:27 -0400

    order model added, imgURL field added to Furniture model tested and functioned

--- furnitures_api/models.py
+++ furnitures_api/models.py
@@ -7,6 +7,7 @@ class Furniture(models.Model):
     #the ForeignKey help to set one to many relationship, on_delete set to null to aviod the child elements getting delete if the parent element get deleted
     name = models.CharField(max_length = 32)
     img = models.ImageField(null = True, blank = True )
+    imgURL = models.TextField (null = True, blank = True)
     color = models.CharField(max_length = 20, default = 'white')
     category = models.CharField(max_length = 32)
     price = models.IntegerField()
@@ -14,5 +15,17 @@ class Furniture(models.Model):
     availability = models.BooleanField()
 
 
-    def __str__(self):
+    def __str__(self): #this will display the items in the database by the name instead of just the furniture 1, furniture 2, etc...
         return self.name
+
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete = models.SET_NULL, null = True)
+    paymentMethod = models.CharField(max_length = 200, null = True, blank = True)
+    taxPrice = models.DecimalField(max_digits = 4, decimal_places = 2, null = True, blank = True)
+    shippingPrice = models.DecimalField(max_digits = 4, decimal_places = 2, null = True, blank = True)
+    totalPrice  = models.DecimalField(max_digits = 4, decimal_places = 2, null = True, blank = True)
+    isPaid = models.BooleanField(default = False)
+    paidAt = models.DateTimeField (auto_now_add = False, null = True, blank = True)
+    isDelivered = models.BooleanField(default = False)
+    deliveredAt = models.DateTimeField (auto_now_add = False, null = True, blank = True)
