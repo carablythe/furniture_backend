503e1c485270d565ff69c930d2545ec56306438e
Victoria Le <victoriale@Victorias-MacBook-Pro.local>
2022-03-17 14:54:28 -0400

    Order, OrderItems and Shipping tables created with relationship link together

--- furnitures_api/models.py
+++ furnitures_api/models.py
@@ -29,3 +29,30 @@ class Order(models.Model):
     paidAt = models.DateTimeField (auto_now_add = False, null = True, blank = True)
     isDelivered = models.BooleanField(default = False)
     deliveredAt = models.DateTimeField (auto_now_add = False, null = True, blank = True)
+
+
+    def __str__(self):
+        return str(self.createdAt)
+
+class OrderItem (models.Model):
+    product = models.ForeignKey(Furniture, on_delete = models.SET_NULL, null = True)
+    order = models.ForeignKey(Order, on_delete = models.SET_NULL, null = True)
+    name = models.CharField(max_length = 200, null = True, blank = True)
+    qty = models.IntegerField(null = True, blank = True, default = 0)
+    price  = models.DecimalField(max_digits = 4, decimal_places = 2, null = True, blank = True)
+    img = models.TextField (null = True, blank = True)
+
+    def __str__(self):
+        return str(self.name)
+
+class ShippingAddress (models.Model):
+    order = models.OneToOneField(Order, on_delete = models.CASCADE, null = True, blank = True)
+     #cascade means s.o deleted their order the shipping address will be gone with it
+    address = models.CharField(max_length = 200, null = True, blank = True)
+    city = models.CharField(max_length = 200, null = True, blank = True)
+    postalCode = models.CharField(max_length = 5, null = True, blank = True)
+    state = models.CharField(max_length = 2, null = True, blank = True)
+    shippingPrice = models.DecimalField(max_digits = 4, decimal_places = 2, null = True, blank = True)
+
+    def __str__(self):
+        return str(self.address)
