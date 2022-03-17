516abdb39729b41f65c7fce7c49264c98fc508b7
Victoria Le <victoriale@Victorias-MacBook-Pro.local>
2022-03-17 12:27:58 -0400

    tie furniture to User, when create a new furniture admin can select the user this will be later used to do the cart function. Also, now the furniture will be displayed as their name instead of the furniture_1 or furniture_2 and so on

--- furnitures_api/models.py
+++ furnitures_api/models.py
@@ -1,7 +1,10 @@
 from django.db import models
+from django.contrib.auth.models  import User #this model is already existed, how we create the superuser
 
 # Create your models here.
 class Furniture(models.Model):
+    user = models.ForeignKey(User, on_delete = models.SET_NULL, null = True)
+    #the ForeignKey help to set one to many relationship, on_delete set to null to aviod the child elements getting delete if the parent element get deleted
     name = models.CharField(max_length = 32)
     img = models.ImageField(null = True, blank = True )
     color = models.CharField(max_length = 20, default = 'white')
@@ -9,3 +12,7 @@ class Furniture(models.Model):
     price = models.IntegerField()
     quantity = models.IntegerField(default = 1)
     availability = models.BooleanField()
+
+
+    def __str__(self):
+        return self.name
